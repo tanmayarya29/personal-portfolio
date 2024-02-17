2df1d0f5c3901bfca0a4f323e087ab7b34d86592
Tanmay Mishra <tanmayarya29@gmail.com>
2024-02-17 21:56:27 +0530

    v1 test data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import TheGuy from "../components/TheGuy/TheGuy";
-import { Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import { useTypingEffect } from "../hooks/useTypingEffect";
 
 const introduction = {
@@ -22,12 +22,9 @@ const Home = () => {
     delay: 100,
   });
   return (
-    <Stack
-      direction={{ xs: "column", sm: "row" }}
-      justifyContent="center"
-      alignItems="center"
+    <Grid
+      container
       sx={{
-        height: "100vh",
         width: "100%",
         padding: "1rem",
         backgroundColor: (theme) => theme.palette.background.default,
@@ -44,104 +41,106 @@ const Home = () => {
         },
       }}
     >
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-          width: "100%",
-          borderRadius: "10px",
-          padding: "2rem",
-          backgroundColor: "primary.light",
-          // animation
-          animation: "left2right 1s ease-in-out",
-          "@keyframes left2right": {
-            "0%": {
-              transform: "translateX(-100%)",
-              opacity: 0,
-            },
-            "100%": {
-              transform: "translateX(0)",
-              opacity: 1,
-            },
-          },
-        }}
-      >
-        <TheGuy />
-      </Stack>
-      <Stack
-        direction="column"
-        justifyContent="center"
-        alignItems="flex-start"
-        sx={{
-          textAlign: "center",
-          borderRadius: "10px",
-          padding: "5rem",
-          width: "100%",
-          // animation
-          animation: "right2left 1s ease-in-out",
-          "@keyframes right2left": {
-            "0%": {
-              transform: "translateX(100%)",
-              opacity: 0,
-            },
-            "100%": {
-              transform: "translateX(0)",
-              opacity: 1,
-            },
-          },
-        }}
-      >
-        <Typography
+      <Grid item xs={12} md={4}>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
           sx={{
-            fontSize: "2rem",
-            fontWeight: "500",
-            color: "primary.main",
+            width: "100%",
+            borderRadius: "10px",
+            padding: "2rem",
+            backgroundColor: "primary.light",
+            // scale down by 0.7 based on screen size
+            transform: { xs: "scale(0.7)", md: "scale(1)" },
+            // animation: "fadeIn 1s ease-in-out",
+            "@keyframes fadeIn": {
+              "0%": {
+                opacity: 0,
+              },
+              "100%": {
+                opacity: 1,
+              },
+            },
           }}
         >
-          {title}
-        </Typography>
-        <Typography
+          <TheGuy />
+        </Stack>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="flex-start"
           sx={{
-            fontSize: "3rem",
-            fontWeight: "bold",
-            "&::first-letter": {
+            textAlign: "center",
+            borderRadius: "10px",
+            padding: "5rem",
+            width: "100%",
+            animation: "right2left 1s ease-in-out",
+            "@keyframes right2left": {
+              "0%": {
+                opacity: 0,
+                transform: "translateX(100px)",
+              },
+              "100%": {
+                opacity: 1,
+                transform: "translateX(0)",
+              },
+            },
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "2rem",
+              fontWeight: "500",
               color: "primary.main",
-            },
-          }}
-        >
-          {name}
-        </Typography>
-        <Typography
-          sx={{
-            fontSize: "2rem",
-            fontWeight: "100",
-            color: "text.secondary",
-            marginBottom: "1rem",
-            "&::after": {
-              content: "'.'",
-              color: "primary.main",
-              marginLeft: "5px",
-              fontWeight: "700",
-            },
-          }}
-        >
-          {displayText}
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{
-            fontSize: "1.5rem",
-            fontWeight: "300",
-            color: "text.secondary",
-            marginBottom: "1rem",
-            textAlign: "justify",
-          }}
-        >
-          {description}
-        </Typography>
-      </Stack>
-    </Stack>
+            }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "3rem",
+              fontWeight: "bold",
+              "&::first-letter": {
+                color: "primary.main",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "2rem",
+              fontWeight: "100",
+              color: "text.secondary",
+              marginBottom: "1rem",
+              "&::after": {
+                content: "'.'",
+                color: "primary.main",
+                marginLeft: "5px",
+                fontWeight: "700",
+              },
+            }}
+          >
+            {displayText}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              fontSize: "1.5rem",
+              fontWeight: "300",
+              color: "text.secondary",
+              marginBottom: "1rem",
+              textAlign: "justify",
+            }}
+          >
+            {description}
+          </Typography>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
 
